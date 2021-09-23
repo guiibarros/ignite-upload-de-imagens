1f522425c9254805eab432db6b3b12695ba49f66
Guilherme Barros <guilherme@pop-os.localdomain>
2021-09-23 16:02:03 -0300

    Challenge completed :tada:

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,26 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        bgColor="pGray.800"
+        h="100%"
+        maxH="600px"
+        w="100%"
+        maxW="900px"
+        m={5}
+      >
+        <ModalBody px={0} py={0} overflow="hidden">
+          <Image src={imgUrl} h="100%" w="100%" objectFit="cover" />
+        </ModalBody>
+        <ModalFooter py="3" justifyContent="left">
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,27 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam }) => {
+      const response = await api.get('api/images', {
+        params: { after: pageParam },
+      });
+
+      return response.data;
+    },
+    { getNextPageParam: lastPage => lastPage?.after ?? null }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(page => page.data);
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +46,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="10"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
